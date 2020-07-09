b161585e854a84788c0ecf2cb443c4874997d494
Andres Castellanos <andres.castellanos@datia.co>
2020-07-09 03:13:40 -0500

    Primer diseño

---
+++ index.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GradiWeb</title>
+    <link rel="stylesheet" href="src/index.css">
+</head>
+
+<body>
+    <div class="contenedor_pagina">
+        <div class="contenedor_uno">
+            <div class="contenedor_uno_uno">
+                <div class="contenedor_uno_uno_uno"></div>
+                <div class="contenedor_uno_uno_dos"></div>
+            </div>
+            <div class="contenedor_uno_dos">
+                <div class="contenedor_uno_dos_uno"></div>
+                <div class="contenedor_uno_dos_dos"></div>
+            </div>
+            <div class="contenedor_uno_tres">
+                <div class="contenedor_uno_tres_uno">
+                    <div class="contenedor_uno_tres_uno_uno"></div>
+                    <div class="contenedor_uno_tres_uno_uno"></div>
+                    <div class="contenedor_uno_tres_uno_uno"></div>
+                </div>
+                <div class="contenedor_uno_tres_dos">
+                    <div class="contenedor_uno_tres_dos_uno"></div>
+                    <div class="contenedor_uno_tres_dos_uno"></div>
+                    <div class="contenedor_uno_tres_dos_uno"></div>
+                </div>
+            </div>
+        </div>
+        <div class="contenedor_dos">
+            <div class="contenedor_dos_uno">
+                <div class="contenedor_dos_uno_uno"></div>
+                <div class="contenedor_dos_uno_dos">
+                    <div class="contenedor_dos_uno_dos_uno">
+                        <div class="contenedor_dos_uno_dos_uno_uno"></div>
+                        <div class="contenedor_dos_uno_dos_uno_dos"></div>
+                    </div>
+                    <div class="contenedor_dos_uno_dos_uno">
+                        <div class="contenedor_dos_uno_dos_uno_uno"></div>
+                        <div class="contenedor_dos_uno_dos_uno_dos"></div>
+                    </div>
+                    <div class="contenedor_dos_uno_dos_uno">
+                        <div class="contenedor_dos_uno_dos_uno_uno"></div>
+                        <div class="contenedor_dos_uno_dos_uno_dos"></div>
+                    </div>
+                </div>
+            </div>
+            <div class="contenedor_dos_uno">
+                <div class="contenedor_dos_uno_uno"></div>
+                <div class="contenedor_dos_uno_dos">
+                </div>
+            </div>
+            <div class="contenedor_dos_uno">
+                <div class="contenedor_dos_uno_uno"></div>
+                <div class="contenedor_dos_uno_dos">
+                    <div class="contenedor_tres_uno"></div>
+                    <div class="contenedor_tres_dos"></div>
+                </div>
+            </div>
+            <div class="contenedor_dos_dos">
+                <div class="contenedor_dos_dos_uno b1">
+                    <div class="contenedor_uno_tres_uno">
+                        <div class="contenedor_uno_tres_uno_uno"></div>
+                        <div class="contenedor_uno_tres_uno_uno"></div>
+                        <div class="contenedor_uno_tres_uno_uno"></div>
+                    </div>
+                    <div class="contenedor_uno_tres_dos">
+                        <div class="contenedor_uno_tres_dos_uno"></div>
+                        <div class="contenedor_uno_tres_dos_uno"></div>
+                        <div class="contenedor_uno_tres_dos_uno"></div>
+                    </div>
+                </div>
+                <div class="contenedor_dos_dos_dos b2">
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
